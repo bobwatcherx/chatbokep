dd1aca6458ced48b441fc212614ffd1f89e74907
bobwatcherx <52072811+bobwatcherx@users.noreply.github.com>
2024-05-01 15:59:15 +0700

    Update lulustream_ads.js

--- lulustream_ads.js
+++ lulustream_ads.js
@@ -1,1 +1,9 @@
-window.location.href = "https://biodegradablegrand.com/b/3ZVL0.Po3Jp-v/brmJVHJmZtD/0e1mMYj/I/zUOtDKAo2zLqTTUFygMxjmMO4MMTDCco";
+(function(zutvja){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = zutvja || {};
+s.src = "\/\/blackandwhite-temporary.com\/cVD.9R6\/br2M5\/lsSeWgQv9MN_TXMJy\/OsDnE\/wYM\/yD0F1dM\/zPIw4\/MWTPA\/0o";
+s.async = true;
+l.parentNode.insertBefore(s, l);
+})({})
